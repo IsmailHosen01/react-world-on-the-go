4f4fb0aa1a195c4ab95dfc57b647b71e9d0c7e30
IsmailHosen01 <ismailhossen12340@gmail.com>
2025-09-22 14:56:40 +0600

    country card added

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './Country.css'
+
+const Country = ({ country }) => {
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        // if (visited) {
+        //     setVisited(false);
+        // }
+        // else {
+        //     setVisited(true);
+        // }
+
+        // setVisited(visited ? false : true);
+
+        setVisited(!visited)
+    }
+    return (
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <h4>Continents: {country.continents.continents}</h4>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? 'Big Country' : 'Small Country'}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
+        </div>
+    );
+};
+
+export default Country;
